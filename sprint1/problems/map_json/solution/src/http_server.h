b6fb7b6b86ef5e9ffc7e886cef679d4ccfd945cf
Vitaly Pavlov <pvs.mazilla@gmail.com>
2022-11-05 15:10:35 +0300

    Init commit to mapJson.

---
+++ sprint1/problems/map_json/solution/src/http_server.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "sdk.h"
+//
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/strand.hpp>
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+
+namespace http_server {
+
+// Разместите здесь реализацию http-сервера, взяв её из задания по разработке асинхронного сервера
+
+}  // namespace http_server
---
+++ sprint1/problems/map_json/solution/src/json_loader.cpp
@@ -0,0 +1,14 @@
+#include "json_loader.h"
+
+namespace json_loader {
+
+model::Game LoadGame(const std::filesystem::path& json_path) {
+    // Загрузить содержимое файла json_path, например, в виде строки
+    // Распарсить строку как JSON, используя boost::json::parse
+    // Загрузить модель игры из файла
+    model::Game game;
+
+    return game;
+}
+
+}  // namespace json_loader
