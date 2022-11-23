1bd57521916608ffa6ee4a644dc60c2544d91f50
Vitaly Pavlov <pvs.mazilla@gmail.com>
2022-11-24 01:43:43 +0300

    Fixed build process updated minimum required version of boost to 1.80.0 .

--- sprint2/problems/server_logging/solution/src/logging/logger.h
+++ sprint2/problems/server_logging/solution/src/logging/logger.h
@@ -1,11 +1,7 @@
 #pragma once
 #include <boost/log/trivial.hpp>     // для BOOST_LOG_TRIVIAL
 #include <boost/log/core.hpp>        // для logging::core
-#include <boost/log/expressions.hpp> // для выражения, задающего фильтр 
-//#include <boost/log/utility/setup/file.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
 #include <boost/log/utility/setup/console.hpp>
-//#include <boost/log/utility/manipulators/add_value.hpp>
 
 #include "logging_data_storage.h"
 
