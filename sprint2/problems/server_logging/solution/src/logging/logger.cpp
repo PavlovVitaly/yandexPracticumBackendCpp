85dfe02f13fff2d7175107de2acc348094df8a2a
Vitaly Pavlov <pvs.mazilla@gmail.com>
2022-11-24 16:00:46 +0300

    Set cout.

--- sprint2/problems/server_logging/solution/src/logging/logger.cpp
+++ sprint2/problems/server_logging/solution/src/logging/logger.cpp
@@ -9,7 +9,8 @@ void StringFormatter(logging::record_view const& rec, logging::formatting_ostrea
 
 void InitLogger() {
     boost::log::add_console_log( 
-        std::clog,
+        std::cout,
+        keywords::auto_flush = true,
         boost::log::keywords::format = &StringFormatter//"%Message%"
     );
 };
