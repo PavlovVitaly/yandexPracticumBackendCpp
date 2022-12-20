20e0e99f20acbd09acc41b627576ba49eb621710
Vitaly Pavlov <pvs.mazilla@gmail.com>
2022-12-20 23:14:55 +0300

    gather-tests init commit.

---
+++ sprint3/problems/gather-tests/solution/tests/collision-detector-tests.cpp
@@ -0,0 +1,61 @@
+#define _USE_MATH_DEFINES
+#include <catch2/catch_test_macros.hpp>
+
+#include "../src/collision_detector.h"
+
+#include <string>
+#include <memory>
+
+// Напишите здесь тесты для функции collision_detector::FindGatherEvents
+
+namespace {
+    const std::string TAG = "[FindGatherEvents]";
+}
+
+namespace collision_detector {
+
+class ItemGathererProviderImpl: public ItemGathererProvider {
+public:
+    virtual ~ItemGathererProviderImpl() = default;
+    
+    size_t ItemsCount() const {
+        return items_.size();
+    };
+
+    Item GetItem(size_t idx) const override{
+        return items_[idx];
+    };
+
+    void AddItem(Item item) {
+        items_.push_back(std::move(item));
+    };
+
+    size_t GatherersCount() const override{
+        return gatherers_.size();
+    };
+
+    Gatherer GetGatherer(size_t idx) const override{
+        return gatherers_[idx];
+    };
+
+    void AddGatherer(Gatherer gatherer) {
+        gatherers_.push_back(std::move(gatherer));
+    };
+private:
+    std::vector<Item> items_;
+    std::vector<Gatherer> gatherers_;
+};
+
+}
+
+using namespace std::literals;
+
+TEST_CASE("Gather collect one item moving on x-axis", TAG) {
+    collision_detector::Item item{{12.5, 0}, 0.6};
+    collision_detector::Gatherer gatherer{{0, 0}, {22.5, 0}, 0.6};
+    collision_detector::ItemGathererProviderImpl provider;
+    provider.AddItem(item);
+    provider.AddGatherer(gatherer);
+    auto events = collision_detector::FindGatherEvents(provider);
+    CHECK(!events.empty());
+}
