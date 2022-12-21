c27298e517b078fe0118d1676801e8409b74a899
Vitaly Pavlov <pvs.mazilla@gmail.com>
2022-12-21 13:14:16 +0300

    cmake_multi test with old workflow.

--- sprint3/problems/gather-tests/solution/tests/collision-detector-tests.cpp
+++ sprint3/problems/gather-tests/solution/tests/collision-detector-tests.cpp
@@ -1,5 +1,6 @@
 #define _USE_MATH_DEFINES
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
 
 #include "../src/collision_detector.h"
 
@@ -51,11 +52,14 @@ private:
 using namespace std::literals;
 
 TEST_CASE("Gather collect one item moving on x-axis", TAG) {
+    using Catch::Matchers::WithinRel;
     collision_detector::Item item{{12.5, 0}, 0.6};
     collision_detector::Gatherer gatherer{{0, 0}, {22.5, 0}, 0.6};
     collision_detector::ItemGathererProviderImpl provider;
     provider.AddItem(item);
     provider.AddGatherer(gatherer);
     auto events = collision_detector::FindGatherEvents(provider);
-    CHECK(!events.empty());
+    CHECK(events.size() == 1);
+    CHECK(events[0].sq_distance == 0);
+    CHECK_THAT(events[0].time, WithinRel((item.position.x/gatherer.end_pos.x), 1e-9)); 
 }
